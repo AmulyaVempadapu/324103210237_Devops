f630f2ebda3dd304b8cfa325688a00b72d02c3a4
AmulyaVempadapu <amulyavempadapu@gmail.com>
2026-07-23 13:42:24 +0530

    Update prg2.java

--- prg2.java
+++ prg2.java
@@ -3,8 +3,8 @@ public class prg2 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = 25;
+        int b = 40;
 
         System.out.println("Sum = " + (a + b));
     }
